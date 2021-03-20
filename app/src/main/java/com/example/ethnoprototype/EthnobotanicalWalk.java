373973e9f94914012eafae351b69debdd23cbec5
rchitakunye7@gmail.com <rchitakunye7@gmail.com>
2021-03-20 19:11:50 +0200

    fixed vid categorization interface now working on remaining 3

---
+++ app/src/main/java/com/example/ethnoprototype/EthnobotanicalWalk.java
@@ -0,0 +1,14 @@
+package com.example.ethnoprototype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EthnobotanicalWalk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ethnobotanical_walk);
+    }
+}
---
+++ app/src/main/java/com/example/ethnoprototype/VisitorPage.java
@@ -0,0 +1,14 @@
+package com.example.ethnoprototype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisitorPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visitor_page);
+    }
+}
