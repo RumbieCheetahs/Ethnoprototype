0e6145a3e5a57c5b81a9466ac21619a03e1ded15
unknown <rchitakunye7@gmail.com>
2021-03-20 09:16:20 +0200

    Initial commit third screen not working properly

---
+++ app/src/main/java/com/example/ethnoprototype/vid_categorisaton.java
@@ -0,0 +1,14 @@
+package com.example.ethnoprototype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class vid_categorisaton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vid_categorisaton);
+    }
+}
