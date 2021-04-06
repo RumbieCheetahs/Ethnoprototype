351631953d957d8dd3d768eafa24a0b9b70a07d7
Blessing Sibanda <banQomania@users.noreply.github.com>
2021-04-06 12:54:58 +0200

    - Add UncategorisedItemsActivity.java recyclerview

--- app/src/main/java/com/example/ethnoprototype/MainActivity.java
+++ app/src/main/java/com/example/ethnoprototype/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.ethnoprototype;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    Button visitor;
+    ImageButton ethnographer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        visitor = findViewById(R.id.btn_visitor);
+
+        ethnographer = findViewById(R.id.btn_paraecologist);
+        visitor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, VisitorPage.class));
+            }
+        });
+
+        ethnographer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
