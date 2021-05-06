3f393ca54d28a8895a7e51114cbd02f807ee152e
Blessing Sibanda <banQomania@users.noreply.github.com>
2021-05-06 12:11:34 +0200

    - Add MapFragment

---
+++ app/src/main/java/com/example/ethnoprototype/MapsFragment.java
@@ -0,0 +1,80 @@
+package com.example.ethnoprototype;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ethnoprototype.data.AppDatabase;
+import com.example.ethnoprototype.data.CategoryAssignedResource;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+public class MapsFragment extends AppCompatActivity implements OnMapReadyCallback{
+
+    private GoogleMap mMap;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_maps);
+//        MapFragment
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+
+        List<CategoryAssignedResource> resourceList = AppDatabase.getAppDatabase(MapsFragment.this).assignedResourceDAO().getAll();
+        if(resourceList.size() != 0) {
+            for (CategoryAssignedResource resource : resourceList) {
+                LatLng latLng = new LatLng(resource.videoId.latitude, resource.videoId.longitude);
+                mMap.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .title(""+resource.plantName)).showInfoWindow();
+            }
+        }
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")).showInfoWindow();
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater,
+//                             @Nullable ViewGroup container,
+//                             @Nullable Bundle savedInstanceState) {
+//        return inflater.inflate(R.layout.fragment_maps, container, false);
+//    }
+
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//        SupportMapFragment mapFragment =
+//                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+//        if (mapFragment != null) {
+//            mapFragment.getMapAsync(callback);
+//        }
+//    }
+}
--- app/src/main/java/com/example/ethnoprototype/VisitorPage.java
+++ app/src/main/java/com/example/ethnoprototype/VisitorPage.java
@@ -2,13 +2,30 @@ package com.example.ethnoprototype;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.MapFragment;
 
 public class VisitorPage extends AppCompatActivity {
+
+    Button btnEthnoBotanicalWalk, btnSearchByName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visitor_page);
+
+
+        btnEthnoBotanicalWalk = findViewById(R.id.searchbycategory);
+
+        btnEthnoBotanicalWalk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(VisitorPage.this, MapsFragment.class));
+            }
+        });
     }
 }
